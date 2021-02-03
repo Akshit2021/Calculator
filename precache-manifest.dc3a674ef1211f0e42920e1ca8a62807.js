ddd8395176f1c66eb290e95303ed2730af8df39b
Akshit aggarwal <akshit.aggarwal@capdomain.com>
2021-02-03 16:56:48 +0530

    Updates

---
+++ precache-manifest.dc3a674ef1211f0e42920e1ca8a62807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fed89b482d78e9cba0b7865da3e4040",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "6976495e3d8c04797561",
+    "url": "/Calculator/static/css/main.a81285a9.chunk.css"
+  },
+  {
+    "revision": "d23d9874216c33e25ddd",
+    "url": "/Calculator/static/js/2.b46ed1f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Calculator/static/js/2.b46ed1f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6976495e3d8c04797561",
+    "url": "/Calculator/static/js/main.d0bd66bf.chunk.js"
+  },
+  {
+    "revision": "f8aea4b98e981a1e0082",
+    "url": "/Calculator/static/js/runtime-main.ffd4c8ba.js"
+  }
+]);
